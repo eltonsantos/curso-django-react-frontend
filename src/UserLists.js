a51abba1d395473f1b54871e4b71937903e260f0
Elton Santos <eltonax@hotmail.com>
2020-06-22 02:54:49 -0300

    Adding token and additional headers to the fetch call

--- src/UserLists.js
+++ src/UserLists.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ListComponent from './ListComponent';
 
-export default function UserLists(){
-    return (
-        <div>
-            <ListComponent listName={"Minha Lista"} />
-            <ListComponent listName={"Minha Lista 2"} />
-        </div>
-    )
+export default class UserLists extends React.Component {
+    
+    state = { list: null, loading: true }
+
+    async componentDidMount(){
+        const config = {
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        }
+        config.headers["Authorization"] = 'Token fbc49a4bd6b0d54abb01c9310b1212ff3021f21a'
+
+        var url = "http://localhost:8000/list/";
+        const response = await fetch(url, config);
+        const data = await response.json();
+        console.log(data);
+        this.setState({lists: data, loading: false});
+    }
+
+    render(){
+        return (
+            <div>
+                <ListComponent listName={"Minha Lista"} />
+                <ListComponent listName={"Minha Lista 2"} />
+            </div>
+        )
+    }
 }
